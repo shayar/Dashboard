c344efaf85e005079444bb78027e8f6e55b13a8b
shayar <shayarshrestha23@gmail.com>
2019-03-20 11:05:02 +0545

    Seeding Data to DB

--- Advantage.API/Startup.cs
+++ Advantage.API/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Advantage.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,11 +27,13 @@ namespace Advantage.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApiContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddTransient<dataseed>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, dataseed seed)
         {
             if (env.IsDevelopment())
             {
@@ -41,7 +45,10 @@ namespace Advantage.API
             }
 
             app.UseHttpsRedirection();
+            seed.seedData(20, 1000);
             app.UseMvc();
+
+            
         }
     }
 }
--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,31 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Order } from 'src/app/shared/Order';
 
+
+
 @Component({
   selector: 'app-section-orders',
   templateUrl: './section-orders.component.html',
   styleUrls: ['./section-orders.component.css']
 })
-export class SectionOrdersComponent implements OnInit {
 
+
+export class SectionOrdersComponent implements OnInit {
+  orders: Order = [
+// tslint:disable-next-line: max-line-length
+    {id: 1, customer: {id: 6, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'}, total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
+// tslint:disable-next-line: max-line-length
+    {id: 2, customer:{id: 7, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
+// tslint:disable-next-line: max-line-length
+    {id: 3, customer:{id: 8, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
+// tslint:disable-next-line: max-line-length
+    {id: 4, customer:{id: 9, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
+// tslint:disable-next-line: max-line-length
+    {id: 5, customer:{id: 10, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)}  
+  ];
   constructor() { }
 
-  orders: Order = [
-    {id: 1, customer:
-      {id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
-       total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
-    {id: 2, customer:
-       {id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
-        total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
-    {id: 3, customer:
-       {id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
-        total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
-    {id: 4, customer:
-       {id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
-        total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
-    {id: 5, customer:
-       {id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
-        total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)}  ];
+  
   ngOnInit() {
   }
 
