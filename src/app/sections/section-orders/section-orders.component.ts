4c27a6953e169a81dff42a4add1185e6f2e3d3ce
shayar <shayarshrestha23@gmail.com>
2019-03-20 11:05:47 +0545

    Updated Order.ts

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -13,19 +13,18 @@ import { Order } from 'src/app/shared/Order';
 export class SectionOrdersComponent implements OnInit {
   orders: Order = [
 // tslint:disable-next-line: max-line-length
-    {id: 1, customer: {id: 6, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'}, total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
+    {id: 1, customer: { id: 6, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'}, total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
 // tslint:disable-next-line: max-line-length
-    {id: 2, customer:{id: 7, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
+    {id: 2, customer: { id: 7, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'}, total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
 // tslint:disable-next-line: max-line-length
-    {id: 3, customer:{id: 8, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
+    {id: 3, customer: { id: 8, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'}, total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
 // tslint:disable-next-line: max-line-length
-    {id: 4, customer:{id: 9, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
+    {id: 4, customer: { id: 9, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'}, total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)},
 // tslint:disable-next-line: max-line-length
-    {id: 5, customer:{id: 10, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)}  
+    {id: 5, customer: { id: 10, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'}, total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 2)}
   ];
   constructor() { }
 
-  
   ngOnInit() {
   }
 
